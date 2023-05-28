99cc194b51bb5a150b55c9364a619e52275e57f1
Ahmad1812 <ahmadimran1812@gmail.com>
2023-05-28 20:26:37 +0500

    icon changes

--- resources/views/admin/main.blade.php
+++ resources/views/admin/main.blade.php
@@ -73,7 +73,7 @@
 
               <div class="d-flex align-items-center">
                 <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                  <i class="bi bi-camera-people"></i>
+                  <i class="bi-camera-video"></i>
                 </div>
                 <div class="ps-3">
                   <h6>{{$courses}}</h6>
